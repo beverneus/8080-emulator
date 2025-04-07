6608e2ed4e8403fd5e3cd7cf658b26347ea24993
Lars De Volder <36573562+beverneus@users.noreply.github.com>
2025-04-08 01:00:38 +0200

    Refactor decode to use individual opcodes

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -32,7 +32,6 @@ uint16_t Cpu::read16atPC() {
 
 int Cpu::decode() {
     const uint8_t opcode = memory.read(regs.PC);
-    const uint8_t group = (opcode & 0b11000000) >> 6;
 
     uint16_t address;
     uint8_t value8;
@@ -40,96 +39,306 @@ int Cpu::decode() {
     // uint16_t value16;
 
     regs.PC += 1; // increment PC to avoid repetitive code
-
-    switch (group) { // First check some groups of opcodes we can generalize
-        case 0b01: // MOV opcodes
-        { 
-            const uint8_t SSS = opcode & 0b00000111;        // Source 
-            const uint8_t DDD = (opcode & 0b00111000) >> 3; // Destination
-            uint8_t *dest = regs.getSingleRegister(DDD);
-            uint8_t *source = regs.getSingleRegister(SSS);
-            if (SSS == 0b110) { // Move from memory, address in register pair HL, value in register DDD
-                *dest = memory.read(regs.readHL());
-                return 2; // 2 cycles
-            } else if (DDD == 0b110) { // Move to memory, address in register pair HL, value in register SSS
-                memory.write(regs.readHL(), *source);
-                return 2; // 2 cycles
-            } else { // Move, register SSS to register DDD
-                *dest = *source; 
-                return 1; // 1 cycle
-            }
-        }
-        case 0b00: // Data transfer (without MOV and XCHG)
-        {   
-            // Move Immediate
-            if ((opcode & 0b111) == 0b110) {  
-                uint8_t value = memory.read(regs.PC++);
-                const uint8_t DDD = (opcode & 0b00111000) >> 3; // Destination
-                uint8_t *dest = regs.getSingleRegister(DDD);
-                if (dest == nullptr) { // Move to memory immediate
-                    assert(opcode == 0b00110110); // Assert this is actually the correct opcode 
-                    memory.write(regs.readHL(), value);
-                    return 3;
-                } else { // Move immediate
-                    *dest = value;
-                    return 2;
-                }
-            // Load register pair immediate
-            } else if ((opcode & 0b1111) == 0b0001) {
-                uint8_t RP = (opcode & 0b00110000);
-                uint16_t value = read16atPC();
-                regs.setRP(RP, value);
-                return 3;
-            // Load accumulator indirect
-            } else if ((opcode & 0b1111) == 0b1010 && (opcode & 0b00100000) >> 5 == 0b0) {
-                uint8_t RP = (opcode & 0b00110000);
-                uint16_t address = regs.readRP(RP);
-                regs.A = memory.read(address);
+    
+    switch (opcode) {
+        { // MOV
+            // destination B
+            case 0x40:
+                regs.B = regs.B;
+                return 1;
+            case 0x41:
+                regs.B = regs.C;
+                return 1;
+            case 0x42:
+                regs.B = regs.D;
+                return 1;
+            case 0x43:
+                regs.B = regs.E;
+                return 1;
+            case 0x44:
+                regs.B = regs.H;
+                return 1;
+            case 0x45:
+                regs.B = regs.L;
+                return 1;
+            case 0x46:
+                address = regs.readHL();
+                regs.B = memory.read(address);
                 return 2;
-            } else if ((opcode & 0b1111) == 0b0010 && (opcode & 0b00100000) >> 5 == 0b0) {
-                uint8_t RP = (opcode & 0b00110000);
-                uint16_t address = regs.readRP(RP);
+            case 0x47:
+                regs.B = regs.A;
+                return 1;
+            // destination C
+            case 0x48:
+                regs.C = regs.B;
+                return 1;
+            case 0x49:
+                regs.C = regs.C;
+                return 1;
+            case 0x4A:
+                regs.C = regs.D;
+                return 1;
+            case 0x4B:
+                regs.C = regs.E;
+                return 1;
+            case 0x4C:
+                regs.C = regs.H;
+                return 1;
+            case 0x4D:
+                regs.C = regs.L;
+                return 1;
+            case 0x4E:
+                address = regs.readHL();
+                regs.C = memory.read(address);
+                return 2;
+            case 0x4F:
+                regs.C = regs.A;
+                return 1;
+            // destination D
+            case 0x50:
+                regs.D = regs.B;
+                return 1;
+            case 0x51:
+                regs.D = regs.C;
+                return 1;
+            case 0x52:
+                regs.D = regs.D;
+                return 1;
+            case 0x53:
+                regs.D = regs.E;
+                return 1;
+            case 0x54:
+                regs.D = regs.H;
+                return 1;
+            case 0x55:
+                regs.D = regs.L;
+                return 1;
+            case 0x56:
+                address = regs.readHL();
+                regs.D = memory.read(address);
+                return 2;
+            case 0x57:
+                regs.D = regs.A;
+                return 1;
+            // destination E
+            case 0x58:
+                regs.E = regs.B;
+                return 1;
+            case 0x59:
+                regs.E = regs.C;
+                return 1;
+            case 0x5A:
+                regs.E = regs.D;
+                return 1;
+            case 0x5B:
+                regs.E = regs.E;
+                return 1;
+            case 0x5C:
+                regs.E = regs.H;
+                return 1;
+            case 0x5D:
+                regs.E = regs.L;
+                return 1;
+            case 0x5E:
+                address = regs.readHL();
+                regs.E = memory.read(address);
+                return 2;
+            case 0x5F:
+                regs.E = regs.A;
+                return 1;
+            // destination H
+            case 0x60:
+                regs.H = regs.B;
+                return 1;
+            case 0x61:
+                regs.H = regs.C;
+                return 1;
+            case 0x62:
+                regs.H = regs.D;
+                return 1;
+            case 0x63:
+                regs.H = regs.E;
+                return 1;
+            case 0x64:
+                regs.H = regs.H;
+                return 1;
+            case 0x65:
+                regs.H = regs.L;
+                return 1;
+            case 0x66:
+                address = regs.readHL();
+                regs.H = memory.read(address);
+                return 2;
+            case 0x67:
+                regs.H = regs.A;
+                return 1;
+            // destination L
+            case 0x68:
+                regs.L = regs.B;
+                return 1;
+            case 0x69:
+                regs.L = regs.C;
+                return 1;
+            case 0x6A:
+                regs.L = regs.D;
+                return 1;
+            case 0x6B:
+                regs.L = regs.E;
+                return 1;
+            case 0x6C:
+                regs.L = regs.H;
+                return 1;
+            case 0x6D:
+                regs.L = regs.L;
+                return 1;
+            case 0x6E:
+                address = regs.readHL();
+                regs.L = memory.read(address);
+                return 2;
+            case 0x6F:
+                regs.L = regs.A;
+                return 1;
+            // destination memory address in HL
+            case 0x70:
+                address = regs.readHL();
+                memory.write(address, regs.B);
+                return 2;
+            case 0x71:
+                address = regs.readHL();
+                memory.write(address, regs.C);
+                return 2;        
+            case 0x72:
+                address = regs.readHL();
+                memory.write(address, regs.D);
+                return 2;
+            case 0x73:
+                address = regs.readHL();
+                memory.write(address, regs.E);
+                return 2;
+            case 0x74:
+                address = regs.readHL();
+                memory.write(address, regs.H);
+                return 2;
+            case 0x75:
+                address = regs.readHL();
+                memory.write(address, regs.L);
+                return 2;
+            case 0x76:
+                UnimplementedInstruction(); // HALT
+                return 0;
+            case 0x77:
+                address = regs.readHL();
                 memory.write(address, regs.A);
                 return 2;
-            }
-            break;
-        }
-        default: break;
-    }
-
-    switch (opcode) {
-        case 0x00: //NOP
-            break;
-        // Data transfer
-        case 0b00111010: // Load Accumulator direct
-        {
-            uint16_t value = read16atPC();
-            regs.A = value;
-            return 4;
-        }
-        case 0b00110010: // Store Accumulator direct
-        {
-            uint16_t value = read16atPC();
-            memory.write(value, regs.A);
-            return 4;
-        }
-        case 0b00101010: // Load H and L direct
-        {
-            uint16_t address = read16atPC();
+            // destination A
+            case 0x78:
+                regs.A = regs.B;
+                return 1;
+            case 0x79:
+                regs.A = regs.C;
+                return 1;
+            case 0x7A:
+                regs.A = regs.D;
+                return 1;
+            case 0x7B:
+                regs.A = regs.E;
+                return 1;
+            case 0x7C:
+                regs.A = regs.H;
+                return 1;
+            case 0x7D:
+                regs.A = regs.L;
+                return 1;
+            case 0x7E:
+                address = regs.readHL();
+                regs.A = memory.read(address);
+                return 2;
+            case 0x7F:
+                regs.A = regs.A;
+                return 1;
+        };        
+        { // MVI
+            case 0x06:
+                regs.B = memory.read(regs.PC++);
+                return 2;
+            case 0x0E:
+                regs.C = memory.read(regs.PC++);
+                return 2;
+            case 0x16:
+                regs.D = memory.read(regs.PC++);
+                return 2;
+            case 0x1E:
+                regs.E = memory.read(regs.PC++);
+                return 2;
+            case 0x26:
+                regs.H = memory.read(regs.PC++);
+                return 2;
+            case 0x2E:
+                regs.L = memory.read(regs.PC++);
+                return 2;
+            case 0x36:
+                value8 = memory.read(regs.PC++);
+                memory.write(regs.readHL(), value8);
+                return 3;
+            case 0x3E:
+                regs.A = memory.read(regs.PC++);
+                return 2;
+        };
+        { // LXI
+            case 0x01:
+                regs.setBC(read16atPC());
+                return 3;
+            case 0x11:
+                regs.setDE(read16atPC());
+                return 3;
+            case 0x21:
+                regs.setHL(read16atPC());
+                return 3;
+            case 0x31:
+                regs.SP = read16atPC();
+                return 3;
+        };
+        { // LDA / LDAX
+            case 0x0A:
+                regs.A = memory.read(regs.readBC());
+                return 2;
+            case 0x1A:
+                regs.A = memory.read(regs.readDE());
+                return 2;
+            case 0x3A: // LDA
+                regs.A = memory.read(read16atPC());
+                return 4;
+        };
+        { // STA / STAX
+            case 0x02:
+                memory.write(regs.readBC(), regs.A);
+                return 2;
+            case 0x12:
+                memory.write(regs.readDE(), regs.A);
+                return 2;
+            case 0x32:
+                memory.write(read16atPC(), regs.A);
+                return 4;
+        };
+        { // HL stuff
+        case 0x2A: // LHLD
+            address = read16atPC();
             regs.L = memory.read(address);
             regs.H = memory.read(address+1);
             return 5;
-        }
-        case 0b11101011: // Exchange H and L with D and E
-        {
-            uint16_t temp = regs.readHL();
+        case 0x22: // SHLD
+            address = read16atPC();
+            memory.write(address, regs.L);
+            memory.write(address+1, regs.H);
+            return 5;
+        case 0xEB: // XCHG
+            temp16 = regs.readHL();
             regs.setHL(regs.readDE());
-            regs.setDE(temp);
+            regs.setDE(temp16);
             return 1;
         }
         default:
             UnimplementedInstruction();
-    }
-    return 1; // most instructions are 1 cycle long
-
+            return 0;
+    }   
 }
