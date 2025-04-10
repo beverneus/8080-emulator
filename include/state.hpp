fe9df2155f5be1e072a5395d26afba527a936d00
Lars De Volder <36573562+beverneus@users.noreply.github.com>
2025-04-11 01:46:49 +0200

    Refactor setFlags method: move declaration to public section of Registers class

--- include/state.hpp
+++ include/state.hpp
@@ -16,7 +16,6 @@ class Registers
     uint16_t readPair(uint8_t high, uint8_t low);
     void setPair(uint8_t *high, uint8_t *low, uint16_t value);
 
-    void setFlags(uint16_t result, bool enableZero = 1, bool enableSign = 1, bool enableParity = 1, bool enableCarry = 1);
 
     public:
         Registers();
@@ -25,6 +24,7 @@ class Registers
         uint8_t A; // Accumulator
         uint8_t F; // Flag register, 5 bits: zero, carry, sign, parity and auxiliary carry
 
+        void setFlags(uint16_t result, bool enableZero = 1, bool enableSign = 1, bool enableParity = 1, bool enableCarry = 1);
         void setFlagsADD(uint16_t result, uint8_t a, uint8_t b, bool carry = 0,
                          bool enableZero = 1, bool enableSign = 1, bool enableParity = 1, bool enableCarry = 1, bool enableAuxCarry = 1);
         void setFlagsSUB(uint16_t result, uint8_t a, uint8_t b, bool borrow = 0,
